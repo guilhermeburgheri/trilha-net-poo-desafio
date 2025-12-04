06f386e927db2c526f7056c3b361b5f7795dd6be
guilhermeburgheri <guilherme.burgheri@hotmail.com>
2025-12-04 18:16:58 -0300

    Funções e classes atualizadas.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,22 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Smartphone nokia = new Nokia("1191234-5678", "Nokia Tijolão", "123456789012345", 32);
+        Console.WriteLine("=== NOKIA ===");
+        nokia.Ligar();
+        nokia.ReceberLigacao();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        Console.WriteLine();
+
+        Smartphone iphone = new Iphone("1198765-4321", "iPhone 17", "987654321098765", 128);
+        Console.WriteLine("=== IPHONE ===");
+        iphone.ReceberLigacao();
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
